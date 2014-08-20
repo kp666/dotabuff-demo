2f76e96f3bf16f353828996086a5f33e438a861a
kp <kpisgod@gmail.com>
2014-08-21 01:04:11 +0530

    fix:  NoMethodError:        undefined method `raw_info' for nil:NilClass

--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -1,23 +1,28 @@
-
 #omniauth-steam =>https://github.com/reu/omniauth-steam
 module Omniauth
 
   module Mock
     def auth_mock
-      OmniAuth.config.mock_auth[:steam] = {
-          :provider => "steam",
-          :uid => "76561198040502687",
-          :info => {
-              :nickname => "Rebel",
-              :name => "Rebel",
-              :location => "",
-              :image => "http://media.steampowered.com/steamcommunity/public/images/avatars/b5/b54980218a6870dfc257f0fae612e3ce3c0dd57d_medium.jpg",
-              :urls => {
-                  :Profile => "http://steamcommunity.com/id/piratekp/"
+      OmniAuth.config.mock_auth[:steam] =
+          OmniAuth::AuthHash.new(
+              {
+                  :provider => "steam",
+                  :uid => "76561198040502687",
+                  :info => {
+                      :nickname => "Rebel",
+                      :name => "Rebel",
+                      :location => "",
+                      :image => "http://media.steampowered.com/steamcommunity/public/images/avatars/b5/b54980218a6870dfc257f0fae612e3ce3c0dd57d_medium.jpg",
+                      :urls => {
+                          :Profile => "http://steamcommunity.com/id/piratekp/"
+                      }
+                  },
+                  :credentials => {},
+                  :extra => {
+                      :raw_info => {}
+                  }
               }
-          },
-          :credentials => {}
-      }
+          )
     end
   end
 
