efbdf2e2ec1896918db2e570551dc6e913d89bf6
kp <kpisgod@gmail.com>
2014-08-21 09:19:20 +0530

    moving matches index to matches controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,7 @@
 require 'will_paginate/array'
 
 class HomeController < ApplicationController
-  before_filter :authenticate_user!
   def index
-    @heading= "Latest Matches in Dota World"
-    if session.key? :current_user
-       history = Dota.history(:start_at_match_id => params[:first_match_id])
-       matches = history.matches
-       @first_match_id = matches.first.id
-    end
-    @pages = matches.paginate(:per_page => 5,:page => params[:page])
-    @match_details  = @pages.collect{|m|Dota.match(m.id)}
-    render :template =>'users/show'
+
   end
 end
--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,12 +1,22 @@
 class MatchesController < ApplicationController
   before_filter :authenticate_user!
 
+  def index
+    @heading= "Latest Matches in Dota World"
+    if session.key? :current_user
+      history = Dota.history(:start_at_match_id => params[:first_match_id])
+      matches = history.matches
+      @first_match_id = matches.first.id
+    end
+    @pages = matches.paginate(:per_page => 5, :page => params[:page])
+    @match_details = @pages.collect { |m| Dota.match(m.id) }
+    render :template => 'users/show'
+  end
 
   def show
-       @match = Dota.match(params[:id])
-       @heading = @match.winner  == :radiant ? "RADIANT VICTORY": "DIRE VICTORY"
-       @players = Match.parse_players(@match)
-    p @players
+    @match = Dota.match(params[:id])
+    @heading = @match.winner == :radiant ? "RADIANT VICTORY" : "DIRE VICTORY"
+    @players = Match.parse_players(@match)
 
 
   end
